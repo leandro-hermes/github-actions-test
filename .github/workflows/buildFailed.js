895094a920a94cb57b35c3c7181fb41187ddfddc
Leandro Hermes Neto <leandro@leandrohermes.net>
2020-10-16 10:50:30 -0300

    build

--- .github/workflows/buildFailed.js
+++ .github/workflows/buildFailed.js
@@ -2,17 +2,19 @@ const http = require('http');
 
 const host = process.argv[2];
 const path = '/frontendCompiled/error';
-const pull_request = process.argv[3];
+const body = {pull_request: process.argv[3]};
 
 console.log('Informando Releaser sobre a falha');
 http.request({
   host,
   path,
+  body,
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
+    'Content-Length': JSON.stringify(body).length,
+    'Host': 'github.com',
   },
-  body: {pull_request},
 }, function (res) {
   console.log(res.statusCode, res.statusMessage);
 });

85438ebdfa4c277409fdfae7a4b83484453461c9
Leandro Hermes Neto <leandro@leandrohermes.net>
2020-10-16 08:50:51 -0300

    build

--- .github/workflows/upload.js
+++ .github/workflows/upload.js
@@ -3,12 +3,15 @@ var fs = require('fs');
 var core = require('@actions/core');
 
 var form = new FormData();
-form.append('pull_request', process.argv[1]);
-form.append('compiled', fs.createReadStream('../../dist/release-prod.zip'));
+form.append('pull_request', process.argv[2]);
+form.append('compiled', fs.readFileSync('../../dist/release-prod.zip'));
 const host = '3244127f7252.ngrok.io';
 const path = '/frontendCompiled';
 
-console.log('Enviando arquivo', +form.size + 'b');
+form.getLength(function (err, l) {
+  console.log('Enviando arquivo', l + 'b');
+});
+
 form.submit({
   host,
   path,

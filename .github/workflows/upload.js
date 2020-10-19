5f19eff21e6bdfb4bf2a6d72b2b0d7134eb920b9
Leandro Hermes Neto <leandro@leandrohermes.net>
2020-10-19 09:34:50 -0300

    build

--- .github/workflows/upload.js
+++ .github/workflows/upload.js
@@ -6,6 +6,12 @@ const path = '/frontendCompiled';
 const filePath = process.argv[3];
 const pr = process.argv[4];
 
+try {
+  fs.existsSync(filePath);
+} catch (e) {
+  return console.error('ERROR: file not found:', filePath);
+}
+
 const form = new FormData();
 form.append('pull_request', pr);
 form.append('compiled', fs.createReadStream(filePath));

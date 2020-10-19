eebf634e146031cf6da29541d7582da813dfd474
Leandro Hermes Neto <leandro@leandrohermes.net>
2020-10-19 17:05:34 -0300

    build

---
+++ .github/actions/upload-file/index.js
@@ -0,0 +1,40 @@
+const FormData = require('form-data');
+const core = require('@actions/core');
+const fs = require('fs');
+
+const host = core.getInput('host');
+const path = core.getInput('path');
+const filePath = core.getInput('filePath');
+const data = core.getInput('data');
+
+console.info('host', host);
+console.info('path', path);
+console.info('data', data);
+
+try {
+  fs.existsSync(filePath);
+  console.info('File found', filePath);
+} catch (e) {
+  return core.setFailed('ERROR: file not found: ' + filePath);
+}
+
+const form = new FormData();
+form.append('file', fs.createReadStream(filePath));
+form.append('data', data);
+
+form.getLength(function (err, l) {
+  console.info('Sending file, size:', l + 'b');
+});
+
+form.submit({host, path}, function (err, res) {
+  if (err) {
+    console.error(err);
+    return core.setFailed('Request failed');
+  }
+
+  console.info('Response', res.statusCode, res.statusMessage);
+
+  if (res.statusCode >= 400 && res.statusCode < 600) {
+    core.setFailed('Request failed');
+  }
+});
